9d5e185ea87620eb97bcb28978d59c3cb5da4178
Acute-Tri-anngle <pptran106@student.hbuhsd.edu>
2024-09-06 11:09:02 -0700

    HHAHHAHH 2

--- Fraction.java
+++ Fraction.java
@@ -1,9 +1,9 @@
 public class Fraction{
     public static void main(String[] args){
-        int num1stNum = 1;
-        int num1stDen = 2;
-        int num2ndNum = 2;
-        int num2ndDen = 5;
+        int num1stNum = 2;
+        int num1stDen = 6;
+        int num2ndNum = -9;
+        int num2ndDen = 10;
 
         System.out.println("The sum of " + num1stNum + "/" + num1stDen + " + " + num2ndNum + "/" + num2ndDen + " = " +  (num1stNum * num2ndDen + num2ndNum * num1stDen) + "/" + (num1stDen * num2ndDen));
     }

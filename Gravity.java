380d032d6a868e239098b08106ce3a89741c2493
Acute-Tri-anngle <pptran106@student.hbuhsd.edu>
2024-09-06 11:07:34 -0700

    HAHHAHHAHAHAHHHHAHAHH

--- Gravity.java
+++ Gravity.java
@@ -1,1 +1,12 @@
+public class Gravity{
+    public static void main(String[] args){
+        final double g = 9.8;
+        int t = 23;
 
+        double h = 0.5 * g * (t * t);
+        double v = g * t;
+
+        System.out.println("The height of the cliff is " + h + " m.");
+        System.out.println("The final velocity of the ball is " + v + " m/s.");
+    }
+}
--- Pyramid.java
+++ Pyramid.java
@@ -1,7 +1,8 @@
-public class Pyramid
-{
-  public static void main(String[] args)
-  {
-    // put code here
+public class Pyramid{
+  public static void main(String[] args){
+      int numBlocks = 69420;
+      final double blockWeight = 2.5;
+      
+      System.out.println("The pyramid weighs " + (numBlocks * blockWeight) + " tons.");
   }
 }
